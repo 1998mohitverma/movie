79f58f5005f274dbdbb4397148517a3700899a29
Mohit Verma <mohitverma201998@gmail.com>
2023-03-29 18:05:04 +0530

    push code

--- movie_project/settings_rail.py
+++ movie_project/settings_rail.py
@@ -1,7 +1,12 @@
 from movie_project.settings import *
 from decouple import config
+import os
 
 SECRET_KEY = config('SECRET_KEY')
-
 ALLOWED_HOSTS = ['web-production-bf16.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-bf16.up.railway.app']
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
